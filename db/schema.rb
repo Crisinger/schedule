de591cf3444e12d0f0a95fdfe3d2f3aa65a52ef5
Charles Risinger <crisinger13@gmail.com>
2017-11-09 01:07:29 +0000

    changed database to postgres, will require additional setup in everybodies ruby and c9 enviroments

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171108231319) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "shifts", force: :cascade do |t|
     t.integer "shift_id"
     t.time "shift_start_time"
