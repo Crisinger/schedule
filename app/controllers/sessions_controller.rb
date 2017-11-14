6fd525129f627c751a4c3de449cfc67d1acc842c
Nathan D Smith <nathan-d-smith@uiowa.edu>
2017-11-14 23:05:40 +0000

    Final changes to user and session views for sprint 2. Still need to complete tests.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:email])
-    #user = User.find_by_user_id(params[:user][:user_id])
     if user && user.authenticate(params[:session][:password])
-      #sign in and redirect to show page
       cookies.permanent[:session_token]= user.session_token
-      # if admin is true -> redirect administrator path
       if user.user_administrator == true
         redirect_to administrator_session_path
       end
-      # if admin is false -> redirect employee path
       else
         redirect_to employee_session_path
       end
