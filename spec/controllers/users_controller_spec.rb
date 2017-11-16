e90628cc7fdebb8449a25a0f55a905c7a06c6345
Charles Risinger <crisinger13@gmail.com>
2017-11-16 21:39:06 +0000

    added pending tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-    pending
+    describe "CREATE user" do
+        it "should creates an object" do
+            pending
+        end
+        it "should flashes an error upon duplicate user-id" do
+            pending
+        end
+        it "should flashes an error upon duplicate email" do
+            pending
+        end
+        it "should flashes a success message upon succesful creation" do
+            pending
+        end
+    end
+    
+    describe "UPDATE user" do
+        pending
+    end
+    describe "DESTROY user" do
+        pending
+    end
+    describe "SHOW user" do
+        pending
+    end
 end
