f58da2157f60656d99ac6f3cac199435fc7ee829
Nathan D Smith <nathan-d-smith@uiowa.edu>
2017-11-28 23:54:05 +0000

    changed seed file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_current_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_current_user, only: [:edit, :show, :administrator, :employee, :new, :create, :update, :destroy,]
   
   def user_params
     params.require(:user).permit(:user_id, :user_first_name, :user_last_name, :email, :password, :password_confirmation, :user_administrator, :user_priority, :user_phone_number)
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
     @user = User.find(id)
   end
   
-  def index
-    @users = User.all
+  def show
+    id = params[:id]
+    @user = User.find(id)
   end
   
   def administrator
@@ -37,17 +38,12 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         flash[:notice] = "#{@user.user_first_name} #{@user.user_last_name}'s account was successfully created."
-        redirect_to administrator_user_path(@user) and return
+        redirect_to administrator_user_path(@current_user) and return
       else
         flash[:notice] = "Something went wrong while trying to create a new account. Please try again"
         redirect_to new_user_path(@user) and return
       end 
     end  
-  end
-  
-  def show
-    id = params[:id]
-    @user = User.find(id)
   end
 
   def update
@@ -55,7 +51,7 @@ class UsersController < ApplicationController
     @user = User.find(id)
     if @user.update_attributes(user_params)
       flash[:notice] = "#{@user.user_first_name} #{@user.user_last_name}'s account was successfully updated."
-      redirect_to administrator_user_path(@user)
+      redirect_to administrator_user_path(@current_user)
     else
       flash[:notice] = "Something happend while trying to update #{@user.user_first_name} #{@user.user_last_name}'s account."
       redirect_to user_path(@user)
@@ -67,7 +63,7 @@ class UsersController < ApplicationController
     @user = User.find(id)
     if @user.destroy
       flash[:notice] = "#{@user.user_first_name} #{@user.user_last_name}'s account was successfully deleted."
-      redirect_to administrator_user_path(@user)
+      redirect_to administrator_user_path(@current_user)
     else
       flash[:notice] = "Something happened while trying to delete #{@user.user_first_name} #{@user.user_last_name}'s account."
       redirect_to user_path(@user)
