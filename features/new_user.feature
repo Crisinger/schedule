0956dcb07f1f88d0669a392cb1b42a3dc7f88e46
Nathan D Smith <nathan-d-smith@uiowa.edu>
2017-12-09 05:26:04 +0000

    create/edit/manage user test are working

--- features/new_user.feature
+++ features/new_user.feature
@@ -1,16 +1,13 @@
 Feature: administrator can create new user accounts
   
 Scenario: create a new user account
-  Given the following users have been added
-  | id  | firstName | last  | admin | priority  | phone           | email       | password  |
-  | 1   | Joe       | Admin | true  | 5         | (123) 456-7890  | 123@abc.com | 12345678  |
-  | 2   | Bill      | Nye   | false | 1         | (098) 765-4321  | abc@123.com | 12345678  |
   Given I am signed in as an adminisrator with the credentials Email: "123@abc.com" and Password: "12345678"
-  Then I should see the text "You Have Succesfully Logged in"
-  When I click the link "Manage Accounts"
+  Then I should see the text "You are logged in as Joe Somebody"
+  When I click "Manage Accounts"
   Then I should see the text "All Employees"
-  When I click the link "Create New Account"
+  When I click "Create New Account"
   Then I should see the text "Create New Employee Account"
   When I have added a new user with the first name "SELT", last name "unchained", email "345@def.com", phone number "(123) 456-7890", user id "12345678", administrative status "false", user priority "1", password "3", password confirmation "12345678"
+  And I click "Create New Account"
   Then I should see a user with the first name "SELT", last name "unchained", email "345@def.com", phone number "(123) 456-7890", user id "12345678", administrative status "false", user priority "1", and password "3"
   
--- features/step_definitions/scheduler_steps.rb
+++ features/step_definitions/scheduler_steps.rb
@@ -1,37 +1,19 @@
-Given("the following users have been added") do |user_table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  #pending # Write code here that turns the phrase above into concrete actions
-  User.create(:user_id =>"4",:user_first_name =>"Joe",:user_last_name =>"Somebody",:user_administrator =>"true",:user_priority =>"5",:user_phone_number =>"(123) 456-7890",:email =>"123@abc.com",:password =>"12345678",:password_confirmation =>"12345678")
-  user_table.hashes.each do |user|
-    User.create(:user_id =>user[:id],:user_first_name =>user[:first],:user_last_name =>user[:last],:user_administrator =>user[:admin],:user_priority =>user[:priority],:user_phone_number =>user[:phone],:email =>user[:email],:password =>user[:passwordD],:password_confirmation =>user[:password])
-    # Each returned user will be a hash representing one row of the users_table
-    # The keys will be the table headers and the values will be the row contents.
-    # Entries can be linked directly to the database with ActiveRecord methods
-    # Add the necessary Active Record call(s) to populate the database.
-  end
-end
-
 Given ("I am signed in as an adminisrator with the credentials Email: {string} and Password: {string}") do |email, password|
   #pending # Write code here that turns the phrase above into concrete actions
+  User.create(:user_id =>"4",:user_first_name =>"Joe",:user_last_name =>"Somebody",:user_administrator =>"true",:user_priority =>"5",:user_phone_number =>"(123) 456-7890",:email =>"123@abc.com",:password =>"12345678",:password_confirmation =>"12345678")
   visit new_session_path
   fill_in 'Email', :with => email
   fill_in 'Password', :with => password
   click_on 'Login'
 end
 
-When("I click the link {string}") do |link|
+When("I click {string}") do |link|
   #pending # Write code here that turns the phrase above into concrete actions
   click_on link
 end
 
-When("I click the button {string}") do |button|
-  pending # Write code here that turns the phrase above into concrete actions
-  click_on button
-end
-
 When("I have added a new user with the first name {string}, last name {string}, email {string}, phone number {string}, user id {string}, administrative status {string}, user priority {string}, password {string}, password confirmation {string}") do |firstName, lastName, email, phoneNumber, userID, adminStatus, priority, password, passwordConfirm|
   #pending # Write code here that turns the phrase above into concrete actions
-  visit new_user_path
   fill_in 'First Name', :with => firstName
   fill_in 'Last Name', :with => lastName
   fill_in 'Email', :with => email
@@ -41,12 +23,10 @@ When("I have added a new user with the first name {string}, last name {string}, 
   select priority, :from => 'User Priority'
   fill_in 'Password', :with => password
   fill_in 'Confirm Password', :with => passwordConfirm
-  click_on 'Create New Account'
 end
 
 When("I have edited a user's information with the first name {string}, last name {string}, email {string}, phone number {string}, user id {string}, administrative status {string}, user priority {string}, password {string}, password confirmation {string}") do |firstName, lastName, email, phoneNumber, userID, adminStatus, priority, password, passwordConfirm|
   #pending # Write code here that turns the phrase above into concrete actions
-  visit new_user_path
   fill_in 'First Name', :with => firstName
   fill_in 'Last Name', :with => lastName
   fill_in 'Email', :with => email
@@ -56,7 +36,6 @@ When("I have edited a user's information with the first name {string}, last name
   select priority, :from => 'User Priority'
   fill_in 'Password', :with => password
   fill_in 'Confirm Password', :with => passwordConfirm
-  click_on 'Update Account'
 end
 
 Then("I should see a user with the first name {string}, last name {string}, email {string}, phone number {string}, user id {string}, administrative status {string}, user priority {string}, and password {string}") do |firstName, lastName, email, phoneNumber, userID, adminStatus, priority, password|
